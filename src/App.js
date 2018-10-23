7039f68c352701049b8b38f5d9d61a719e031dea
abisummers <hello@abisummers.com>
2018-10-23 09:25:25 +0200

    updated project list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Projects from "./components/Project-list";
 
 class App extends Component {
   render() {
@@ -15,33 +16,7 @@ class App extends Component {
           Web Dev student at <a href="https://www.ironhack.com/en">Ironhack</a>,
           Paris
         </p>
-        <p>Some of the projects I've made:</p>
-        <ul>
-          <li>
-            <a href="https://ironhack-sharer.herokuapp.com/">Ironhack Sharer</a>
-          </li>
-          <li>
-            <a href="https://abisummers.com/Project-One/">
-              Tap Tap Revolution!
-            </a>
-          </li>
-          <li>
-            <a href="https://abisummers.com/lab-css-spotify-clone/starter-code/">
-              Spotify landing page clone
-            </a>
-          </li>
-          <li>
-            <a href="https://abisummers.com/todo-list/solution/">Todo list</a>
-          </li>
-          <li>
-            <a href="https://abisummers.com/lab-css-instagram-clone/starter-code/">
-              Instagram landing page clone
-            </a>
-          </li>
-          <li>
-            <a href="https://abisummers.com/mars-rover/">Mars Rover</a>
-          </li>
-        </ul>
+        <Projects />
         <p>
           You can follow me on
           <a href="https://github.com/abisummers">GitHub</a> or
---
+++ src/components/Project-list.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Projects extends Component {
+  render() {
+    return (
+      <>
+        <p>Some of the projects I've made:</p>
+        <ul>
+          <li>
+            <a href="http://www.recyclez-moi.co/">Recyclez-moi</a>
+          </li>
+          <li>
+            <a href="https://ironhack-sharer.herokuapp.com/">Ironhack Sharer</a>
+          </li>
+          <li>
+            <a href="https://abisummers.com/Project-One/">
+              Tap Tap Revolution!
+            </a>
+          </li>
+          <li>
+            <a href="https://abisummers.com/lab-css-spotify-clone/starter-code/">
+              Spotify landing page clone
+            </a>
+          </li>
+          <li>
+            <a href="https://abisummers.com/todo-list/solution/">Todo list</a>
+          </li>
+          <li>
+            <a href="https://abisummers.com/lab-css-instagram-clone/starter-code/">
+              Instagram landing page clone
+            </a>
+          </li>
+          <li>
+            <a href="https://abisummers.com/mars-rover/">Mars Rover</a>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default Projects;
