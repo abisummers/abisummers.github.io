2302c2bc479cbda4dc8566b664397f835617a5c5
abisummers <hello@abisummers.com>
2020-05-02 11:14:20 +0200

    remove footer

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import Main from "./components/Main";
 class App extends Component {
   render() {
     return (
-      <>
-        <Main />
-        <p>
-          You can follow me on{" "}
-          <a target="_" href="https://github.com/abisummers">
-            GitHub
-          </a>{" "}
-          or{" "}
-          <a target="_" href="https://twitter.com/abijsummers">
-            Twitter
-          </a>
-        </p>
-
-      </>
+      <Main />
     );
   }
 }
