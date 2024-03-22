36d56c97dbf83da9eaeca402ae339c0364903938
Haroen <hello@haroen.me>
2024-03-22 20:48:59 +0100

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://abisummers.com',
+  site: "https://abisummers.com",
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
   },
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          fr: "fr",
+        },
+      },
+    }),
+  ],
 });
