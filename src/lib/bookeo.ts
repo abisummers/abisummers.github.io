41879873246a6d53ef324dabf6e4e7a0b7b27b66
Haroen Viaene <hello@haroen.me>
2025-10-01 21:13:43 +0200

    add price

--- src/lib/bookeo.ts
+++ src/lib/bookeo.ts
@@ -1,3 +1,5 @@
+const { BOOKEO_CLIENT, BOOKEO_SERVER } = import.meta.env;
+
 interface BookeoProductsResponse {
   data: BookeoProduct[];
   info: {
@@ -41,11 +43,95 @@ export type BookeoProduct = {
     hours: number;
     minutes: number;
   };
+  /**
+   * Pricing add-ons
+   */
+  defaultRates?: {
+    peopleCategoryId?: string;
+    price: {
+      amount: string;
+      currency: string;
+    };
+  }[];
+  /**
+   * Booking limits (min/max people)
+   */
+  bookingLimits?: {
+    peopleCategoryId?: string;
+    min: number;
+    max: number;
+  }[];
+  /**
+   * Product type
+   */
+  type?: string;
+  /**
+   * Whether members only
+   */
+  membersOnly?: boolean;
+  /**
+   * Additional number options (like extra hours)
+   */
+  numberOptions?: {
+    minValue: number;
+    maxValue: number;
+    defaultValue: number;
+    id: string;
+    name: string;
+    index: number;
+    description?: string;
+    shownToCustomers: boolean;
+    enabled: boolean;
+  }[];
+  /**
+   * Text input options for customization
+   */
+  textOptions?: {
+    required: boolean;
+    id: string;
+    name: string;
+    index: number;
+    description?: string;
+    shownToCustomers: boolean;
+    enabled: boolean;
+  }[];
 };
+
+type BasePrice = {
+  amount: string;
+  currency: string;
+};
+const PRODUCT_BASE_PRICE_MAPPING: Record<string, BasePrice> = {
+  "visit-paris": { amount: "240", currency: "EUR" },
+  montmartre: { amount: "180", currency: "EUR" },
+  "pere-lachaise": { amount: "170", currency: "EUR" },
+  "ile-de-la-cite": { amount: "150", currency: "EUR" },
+};
+
+export async function getProductsWithPricing(): Promise<
+  Omit<BookeoProductsResponse, "data"> & {
+    data: (BookeoProduct & {
+      basePrice?: BasePrice;
+    })[];
+  }
+> {
+  const { data, info } = await getProducts();
+
+  const productsWithPricing = await Promise.all(
+    data.map((product) => {
+      return {
+        ...product,
+        basePrice: PRODUCT_BASE_PRICE_MAPPING[product.productCode],
+      };
+    }),
+  );
+
+  return { data: productsWithPricing, info };
+}
 
 export async function getProducts(): Promise<BookeoProductsResponse> {
   const { data, info } = (await fetch(
-    `https://api.bookeo.com/v2/settings/products?apiKey=${import.meta.env.BOOKEO_CLIENT}&secretKey=${import.meta.env.BOOKEO_SERVER}`,
+    `https://api.bookeo.com/v2/settings/products?apiKey=${BOOKEO_CLIENT}&secretKey=${BOOKEO_SERVER}`,
   )
     .then(async (res) => {
       if (!res.ok) {
