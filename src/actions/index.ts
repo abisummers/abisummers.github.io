a8ca78e9afff33f9da1e779beca4a7621313363a
abisummers <hello@abisummers.com>
2026-02-14 23:14:44 +0100

    feat: enable server actions with netlify

---
+++ src/actions/index.ts
@@ -0,0 +1,13 @@
+import { defineAction } from 'astro:actions';
+import { z } from 'astro/zod';
+
+export const server = {
+  getGreeting: defineAction({
+    input: z.object({
+      name: z.string(),
+    }),
+    handler: async (input) => {
+      return `Hello, ${input.name}!`
+    }
+  })
+}
