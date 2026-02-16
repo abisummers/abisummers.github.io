dbeb565e37f7b9994a8815d1b3a26460e4ebf431
Haroen <hello@haroen.me>
2026-02-16 23:56:54 +0100

    remove action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,13 +1,2 @@
-import { defineAction } from "astro:actions";
-import { z } from "astro/zod";
-
 export const server = {
-  getGreeting: defineAction({
-    input: z.object({
-      name: z.string(),
-    }),
-    handler: async (input) => {
-      return `Hello, ${input.name}!`;
-    },
-  }),
 };
